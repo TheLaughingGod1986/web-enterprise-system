3586bebda9e2718c8ac6c7b12a008b251030bf41
iniestar <ruifurtado24@gmail.com>
2016-02-04 21:50:26 +0000

    Corrected typo bugs and now is running fine :D

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt){
 				reporter: require('jshint-stylish')
 			},
 			target: ['js/global.js']
-		}
+		},
 
 		concat: {
 			// 2. Configuration for concatinating files goes here
@@ -56,22 +56,24 @@ module.exports = function(grunt){
 			css: {
 				files: ['css/*.scss'],
 				tasks: ['sass'],
-				options: spawn: false,
+				options: {
+					spawn: false,
+				}
 			},
 
 			options: {
 				livereload: true,
 			}
-		}
+		},
 
 		sass: {
 			dist: {
 				options: {
-					style: 'compressed'
+					style: 'expanded'
 				},
 
 				files: {
-					'css/build/global.css' : 'css/global.scss'
+					'css/build/global.css': 'css/global.scss'
 				}
 			}
 		}
@@ -83,7 +85,8 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 
 	// 4. Where we tell Grunt what to do when we type "grunt" into terminal.
-	grunt.registerTask('default', ['jshint', 'watch', 'concat', 'uglify', 'sass', 'imagemin']);
+	grunt.registerTask('default', ['jshint', 'watch', 'concat', 'sass', 'uglify', 'imagemin']);
 }
