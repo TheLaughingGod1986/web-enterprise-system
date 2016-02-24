038f46e8bbcda29bcf0c847b56010f0d8f31747f
TheLaughingGod1986 <benoats@gmail.com>
2016-02-24 21:21:30 +0000

    sexy me up

--- application/controllers/UManage_cntrl.php
+++ application/controllers/UManage_cntrl.php
@@ -10,11 +10,12 @@ class UManage_cntrl extends CI_Controller
 
     //Fetch selected user
     function index(){
+
+        $data['main_content'] = 'pages/home_view';
+        $this->load->view('includes/template', $data);
         $id = $this->uri->segment(3);
         $data['all_users'] = $this->UManage_model->get_users();
         $data['single_user'] = $this->UManage_model->get_user_id($id);
-
-        $this->load->view('UManage_view', $data);
     }
 
     //Insert users form
