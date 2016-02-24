bd549855027880eaf8e2d5df19e5bb34e197712d
iniestar <ruifurtado24@gmail.com>
2016-02-24 01:02:44 +0000

    Testing if conflict is solved

--- CodeIgniter/application/config/routes.php
+++ CodeIgniter/application/config/routes.php
@@ -49,10 +49,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-//$route['Delete_controller'] = 'Delete_controller';
+
 //$route['users/(:any)'] = 'users/view/$1';
 //$route['users'] = 'users';
 //$route['(:any)'] = 'pages/view/$1';
+$route['Insert_controller'] = 'Insert_controller';
+$route['Delete_controller'] = 'Delete_controller';
 $route['default_controller'] = 'Update_controller';
 //$route['404_override'] = '';
 //$route['translate_uri_dashes'] = FALSE;
