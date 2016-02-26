4a1ba4e86c5c7b55d82768c48bcf31af8941a2be
TheLaughingGod1986 <benoats@gmail.com>
2016-02-26 15:32:05 +0000

    test

--- application/views/includes/template.php
+++ application/views/includes/template.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>PHP i love you</title>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/build/style.min.css" type="text/css" media="screen" charset="uft-8">
+</head>
+<body>
+
+<div id="wrap">
 <?php $this->view('includes/header'); ?>
 
 <?php $this->view('includes/sidebar'); ?>
@@ -5,3 +16,7 @@
 <?php $this->view($main_content); ?>
 
 <?php $this->view('includes/footer'); ?>
+
+</div>
+</body>
+</html>
