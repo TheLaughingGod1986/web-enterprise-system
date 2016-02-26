5f65b4e3c65616b6069cc7543e4f09f0a029d306
TheLaughingGod1986 <benoats@gmail.com>
2016-02-26 18:27:29 +0000

    fix merge

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -7,8 +7,8 @@ class Pages extends CI_Controller{
 		}
 		$data['title'] = ucfirst($page); //Capitalize the first letter
 
-		$this->load->view('templates/header', $data);
-		$this->load->view('pages/'.$page, $data);
-		$this->load->view('templates/footer', $data);
+		$this->load->view('templates/header', $dataContent);
+		$this->load->view('pages/'.$page, $dataContent);
+		$this->load->view('templates/footer', $dataContent);
 	}
 }
