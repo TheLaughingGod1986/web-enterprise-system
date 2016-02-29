133bf0fc640259ea523d6b5649e4908e8b1735ed
TheLaughingGod1986 <benoats@gmail.com>
2016-02-29 16:07:27 +0000

    new fix

--- application/views/layout/index.php
+++ application/views/layout/index.php
@@ -5,12 +5,22 @@
     <link rel="stylesheet" href="<?php echo base_url(); ?>css/build/style.min.css" type="text/css" media="screen"
           charset="uft-8">
 </head>
+
 <body>
-<?php if($header) echo $header ;?>
-<?php if($left) echo $left ;?>
-<div class="col-xs-8 col-sm-9">
-<?php if($middle) echo $middle ;?>
+<div class="container-fluid">
+    <div class="row-fluid">
+        <?php if ($header) echo $header; ?>
+
+        <div class="span2">
+            <?php if ($left) echo $left; ?>
+        </div>
+
+        <div class="span10">
+            <?php if ($middle) echo $middle; ?>
+        </div>
+
+        <?php if ($footer) echo $footer; ?>
+    </div>
 </div>
-<?php if($footer) echo $footer ;?>
 </body>
 </html>
