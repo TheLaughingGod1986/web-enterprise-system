7e0f10cbb8d5ebc47f884068ce6ffb278ea80bae
TheLaughingGod1986 <benoats@gmail.com>
2016-02-29 18:30:23 +0000

    basic frame set

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,1 +1,5 @@
-side bar
+<ul class="nav navbar-inverse">
+    <li role="presentation" class="active"><a href="#">Home</a></li>
+    <li role="presentation"><a href="#">Profile</a></li>
+    <li role="presentation"><a href="#">Messages</a></li>
+</ul>
