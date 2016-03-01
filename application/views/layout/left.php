051d9fa97f87089b979188a011bd836257451cbd
TheLaughingGod1986 <benoats@gmail.com>
2016-03-01 20:27:36 +0000

    add new css

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,4 +1,4 @@
-<ul class="nav navbar-inverse side-bar">
+<ul class="nav navbar-inverse-light side-bar">
     <li role="presentation" class="active"><a href="#">Home</a></li>
     <li role="presentation"><a href="#">Reports</a></li>
     <li role="presentation"><a href="#">Write / View Response</a></li>
