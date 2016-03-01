83aa100d914e60772b3c70dea6e8d29e1e06926e
iniestar <ruifurtado24@gmail.com>
2016-03-01 01:57:50 +0000

    added missing model on login controller

--- application/controllers/Login_cntrl.php
+++ application/controllers/Login_cntrl.php
@@ -4,6 +4,7 @@
         public function __construct(){
             parent::__construct();
             $this->load->library('session');
+            $this->load->model('UManage_model');
         }
 
         public function index(){
