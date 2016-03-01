1b89475585a916fd9a26357827ca8983409ad619
TheLaughingGod1986 <benoats@gmail.com>
2016-03-01 16:38:41 +0000

    test update- with add user

--- application/controllers/UManage_cntrl.php
+++ application/controllers/UManage_cntrl.php
@@ -16,10 +16,9 @@ class UManage_cntrl extends CI_Controller
         $data['single_user'] = $this->UManage_model->get_user_id($id);
 
         //Template importation
-        $dataContent['main_content'] = 'UManage_view';
-        $this->load->view('includes/template', $dataContent);
+        $this->middle = $this->load->view('UManage_view', $data); // passing middle to function. change this for different views.
+        $this->layout();
 
-        $this->load->view('UManage_view', $data);
 
     }
 
