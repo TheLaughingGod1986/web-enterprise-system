06b4b03d0daba1dc3523f9e1e0923d6780986d12
TheLaughingGod1986 <benoats@gmail.com>
2016-03-01 22:06:57 +0000

    add css

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,7 +1,9 @@
 <ul class="nav navbar-inverse-light side-bar">
+    <li>grey line here</li>
     <li role="presentation" class="active"><a href="#">Home</a></li>
     <li role="presentation"><a href="#">Reports</a></li>
     <li role="presentation"><a href="#">Write / View Response</a></li>
+    <li>grey line here</li>
     <li role="presentation"><a href="#">Your Response</a></li>
     <li role="presentation"><a href="#">Update Personal Details</a></li>
     <li role="presentation"><a href="#">Write / View Response</a></li>
