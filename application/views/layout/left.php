d57869aeba1eb4ba78110e54824b4d29d2ba2df1
TheLaughingGod1986 <benoats@gmail.com>
2016-03-02 03:51:38 +0000

    push to master css updates

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,5 +1,5 @@
 
-<ul class="nav navbar-inverse-light side-bar">
+<ul class="nav navbar-inverse-light side-bar li-change">
     <li class="blue"> <img border="0" alt="boo" src="img/greenwich_logo.png" width="100" height="70"></li>
     <li class="grey"></li>
     <li role="presentation" class="active"><a href="#">Home</a></li>
