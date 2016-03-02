413ed4a6551695ede918cf67786e6bc8f0766f74
TheLaughingGod1986 <benoats@gmail.com>
2016-03-02 00:13:07 +0000

    jghuh

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,17 +1,16 @@
     <nav class="navbar navbar-inverse no-margin-bottom top-menu">
-        <div class="navbar-header">
+
+        <div class="navbar-header col-sm-12">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
                     data-target="#bs-example-navbar-collapse-9" aria-expanded="false"><span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" <?php echo anchor('main/index', 'Scrumptious Seven'); ?></a>
         </div>
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-9">
-            <ul class="nav navbar-nav">
-                <li class="active"><a <?php echo anchor('main/index', 'Home'); ?></a></li>
-                <li><a <?php echo anchor('main/about', 'About'); ?></a></li>
-                <li><a <?php echo anchor('main/add', 'Add New User'); ?></a></li>
-            </ul>
+
+        <div class="collapse navbar-collapse col-sm-12" id="bs-example-navbar-collapse-9">
+           <span><h1>text</h1></span>
         </div>
+
     </nav>
 
