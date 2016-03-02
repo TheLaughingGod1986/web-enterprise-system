e2e301bb54a113e9e8e42e915b0740f15bdadf96
TheLaughingGod1986 <benoats@gmail.com>
2016-03-02 00:46:14 +0000

    add css

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,11 +1,13 @@
 <nav class="navbar navbar-inverse no-margin-bottom top-menu">
 
     <div class="col-sm-2">
-            <img border="0" alt="W3Schools" src="img/greenwich_logo.png" width="100" height="100">
+            <img border="0" alt="W3Schools" src="img/greenwich_logo.png" width="100" height="70">
     </div>
 
-    <div class="col-sm-12">
-        <span><h1>External Examiner Portal</h1></span>
+    <div class="col-sm-9">
+        <span class="text-center">
+            <h1>External Examiner Portal</h1>
+        </span>
     </div>
 
 </nav>
