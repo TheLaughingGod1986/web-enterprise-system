9cce8a96d075f8eea43a2aa9130e78609f2f47ee
TheLaughingGod1986 <benoats@gmail.com>
2016-03-02 02:28:33 +0000

    jghuh

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
     <div class="container">
-        <p class="muted credit">Example courtesy <a href="http://martinbean.co.uk">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
+        <p>My milkshake brings all the boys to the yard. And they're like, it's better than yours</p>
     </div>
 </footer>
