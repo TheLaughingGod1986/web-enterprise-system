9355b4dd2fa3b90e54034990845f78ce6ae29221
TheLaughingGod1986 <benoats@gmail.com>
2016-03-02 00:29:59 +0000

    jghuh

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,16 +1,19 @@
-    <nav class="navbar navbar-inverse no-margin-bottom top-menu">
+<nav class="navbar navbar-inverse no-margin-bottom top-menu">
 
-        <div class="navbar-header col-sm-2">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-9" aria-expanded="false"><span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" <?php echo anchor('main/index'); ?> <img src="/img/greenwich_logo.png"></a>
-        </div>
+    <div class="navbar-header col-sm-2">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-9" aria-expanded="false"><span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
+        </button>
 
-        <div class="collapse navbar-collapse col-sm-12" id="bs-example-navbar-collapse-9">
-           <span><h1>text</h1></span>
-        </div>
+        <a <?php echo anchor('main/index'); ?>
+            <img border="0" alt="W3Schools" src="greenwich_logo.png" width="100" height="100">
+        </a>
+    </div>
 
-    </nav>
+    <div class="collapse navbar-collapse col-sm-12" id="bs-example-navbar-collapse-9">
+        <span><h1>text</h1></span>
+    </div>
+
+</nav>
 
