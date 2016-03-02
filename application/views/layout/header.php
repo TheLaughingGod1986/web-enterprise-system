5d60881e234d09d82c94ed9c1d0e96bfd3a9b402
TheLaughingGod1986 <benoats@gmail.com>
2016-03-02 00:15:31 +0000

    jghuh

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,6 +1,6 @@
     <nav class="navbar navbar-inverse no-margin-bottom top-menu">
 
-        <div class="navbar-header col-sm-12">
+        <div class="navbar-header col-sm-2">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
                     data-target="#bs-example-navbar-collapse-9" aria-expanded="false"><span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
