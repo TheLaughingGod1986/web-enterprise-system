d78ce62f19103af8d9ad5ae8414a42f139c4159f
TheLaughingGod1986 <benoats@gmail.com>
2016-03-02 00:59:49 +0000

    jghuh

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -4,7 +4,7 @@
             <img border="0" alt="W3Schools" src="img/greenwich_logo.png" width="100" height="70">
     </div>
 
-    <div class="col-sm-9">
+    <div class="col-sm-10">
         <span class="top-center-text">
             <h1>External Examiner Portal</h1>
         </span>
