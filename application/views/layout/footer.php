1de4a846f61101217021bdab33a4ccc6ea7dbf92
TheLaughingGod1986 <benoats@gmail.com>
2016-03-02 02:30:30 +0000

    jghuh

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,4 +1,4 @@
-<footer class="footer">
+<footer class="footer top-center-text">
     <div class="container">
         <p>My milkshake brings all the boys to the yard. And they're like, it's better than yours</p>
     </div>
