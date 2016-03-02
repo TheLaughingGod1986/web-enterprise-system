092e735885c944573e5fab35ad333bc20a1aa89e
TheLaughingGod1986 <benoats@gmail.com>
2016-03-02 03:48:28 +0000

    push to master css updates

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,11 +1,11 @@
 
 <ul class="nav navbar-inverse-light side-bar">
-    <li class="blue-li"> <img border="0" alt="boo" src="img/greenwich_logo.png" width="100" height="70"></li>
-    <li class="grey_li"></li>
+    <li class="blue"> <img border="0" alt="boo" src="img/greenwich_logo.png" width="100" height="70"></li>
+    <li class="grey"></li>
     <li role="presentation" class="active"><a href="#">Home</a></li>
     <li role="presentation"><a href="#">Reports</a></li>
     <li role="presentation"><a href="#">Write / View Response</a></li>
-    <li class="grey_li"></li>
+    <li class="grey"></li>
     <li role="presentation"><a href="#">Your Response</a></li>
     <li role="presentation"><a href="#">Update Personal Details</a></li>
     <li role="presentation"><a href="#">Write / View Response</a></li>
