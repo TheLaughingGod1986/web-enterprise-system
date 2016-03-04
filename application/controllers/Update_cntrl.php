edd27ccec0b1aefbb8fdefdaef2047e31d55fac5
TheLaughingGod1986 <benoats@gmail.com>
2016-03-04 17:38:00 +0000

    test

--- application/controllers/Update_cntrl.php
+++ application/controllers/Update_cntrl.php
@@ -1,34 +1,34 @@
 <?php
-
-class Update_cntrl extends CI_Controller
-{
+/**
+ * Created by PhpStorm.
+ * User: ruifurtado
+ * Date: 23/02/2016
+ * Time: 11:24
+ */
+class Update_cntrl extends CI_Controller{
     function __construct()
     {
         parent::__construct();
-        $this->load->model('UManage_model');
+        $this->load->model('Update_model');
     }
 
-    function index()
-    {
-        $data['single_user'] = $this->UManage_model->get_user_id(2);
-        $this->load->view('update_view', $data);
+    //Fetch selected user
+    function index(){
+        $id = $this->uri->segment(3);
+        $data['all_users'] = $this->Update_model->get_users();
+        $data['single_user'] = $this->Update_model->get_user_id($id);
+
+        $this->load->view('Update_view', $data);
     }
 
-    function update()
-    {
-        $id = 2;
+    function update_user(){
+        $id = $this->input->post('userID');
         $data = array(
-            'Email' => $this->input->post('Email'),
-            'Password' => $this->input->post('Password'),
-            'First_Name' => $this->input->post('First_Name'),
-            'Last_Name' => $this->input->post('Last_Name'),
-            'Postcode' => $this->input->post('Postcode'),
-            'Telephone' => $this->input->post('Telephone'),
-            'Address' => $this->input->post('Address'),
-            'Title' => $this->input->post('Title')
+            'userName' => $this->input->post('userName'),
+            'pass' => $this->input->post('pass')
         );
-        $this->UManage_model->update_user($id, $data);
-        $this->load->index();
-    }
 
+        $this->Update_model->update_user($id, $data);
+        $this->index();
+    }
 }
