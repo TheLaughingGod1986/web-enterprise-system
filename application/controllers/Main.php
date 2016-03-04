01de66c5080911f02fe086a5b7a26eef2a0ce353
TheLaughingGod1986 <benoats@gmail.com>
2016-03-04 18:54:24 +0000

    ohh noo

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -5,7 +5,7 @@ class Main extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        $this->load->model('update_model');
+        $this->load->model('Update_model');
     }
 
     var $template = array();
