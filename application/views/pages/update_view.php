9771d539527fb552e2de22e9b090b300dcd1c62d
Ben <benoats@gmail.com>
2016-03-04 17:31:47 +0000

    Update update_view.php

--- application/views/pages/update_view.php
+++ application/views/pages/update_view.php
@@ -5,14 +5,14 @@
 
         <ol>
             <?php foreach($all_users as $users): ?>
-                <li><a href="<?php echo base_url().'index.php/Update_controller/index/'.$users->userID;?>"><?php echo $users->userName; ?></a></li>
+                <li><a href="<?php echo base_url().'index.php/Update_cntrl/index/'.$users->userID;?>"><?php echo $users->userName; ?></a></li>
             <?php endforeach; ?>
         </ol>
     </div>
     <div id="details">
         <?php foreach ($single_user as $item):
 
-            echo form_open('Update_controller/update_user');?>
+            echo form_open('Update_cnrtl/update_user');?>
 
             <?php echo form_label('Examiner ID:'); ?> <?php echo form_error('userID'); ?><br />
             <?php echo form_input(array('id' => 'userID', 'name' => 'userID', 'value'=>$item->userID)); ?><br />
