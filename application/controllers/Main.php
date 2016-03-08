ff4b43acf7a3c1d8062b14d65b76b88f926cc531
TheLaughingGod1986 <benoats@gmail.com>
2016-03-08 14:55:26 +0000

    mes

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -8,7 +8,7 @@ class Main extends CI_Controller
         $this->load->model('Update_model');
         $this->load->helper('array');
 
-        $this->load->model('UManage_model');
+//        $this->load->model('UManage_model');
         $id = $this->uri->segment(3);
         $data['all_users'] = $this->UManage_model->get_users();
         $data['single_user'] = $this->UManage_model->get_user_id($id);
