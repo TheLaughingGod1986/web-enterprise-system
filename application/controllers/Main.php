d338ddb9a8d28740dba062ddd547be98c9558e18
TheLaughingGod1986 <benoats@gmail.com>
2016-03-08 16:38:39 +0000

    mes

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -45,8 +45,8 @@ class Main extends CI_Controller
         $this->layout();
 
         $id = 2;
-        $data['all_users'] = $this->Update_model->get_users();
-        $data['single_user'] = $this->Update_model->get_user_id($id);
+        $this->Update_model->get_users();
+        $this->Update_model->get_user_id($id);
 //        echo 'data variable: '.random_element($data22);
 //        $this->middle = 'pages/update_view.php'; // passing middle to function. change this for different views.
 //        $this->layout();
