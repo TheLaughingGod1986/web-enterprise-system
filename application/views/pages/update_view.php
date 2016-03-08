e502927e2461fbf9d60c0c27b24663d18faf4366
TheLaughingGod1986 <benoats@gmail.com>
2016-03-08 16:44:45 +0000

    mes

--- application/views/pages/update_view.php
+++ application/views/pages/update_view.php
@@ -1,7 +1,7 @@
 <div>
     <h1>Manage Users</h1><hr/>
 
-    <div id="userslist" class="col-sm-5">
+
 
         <ol>
             <?php
@@ -9,7 +9,7 @@
                 foreach ($all_users as $users): ?>
                     <li>
                         <?php echo $users->Last_Name.", ".$users->First_Name; ?>
-                        <a href="<?php echo base_url() . 'index.php/UManage_cntrl/index/' . $users->StaffID; ?>">Edit</a>
+                        <a href="<?php echo base_url() . 'index.php/Main/update/' . $users->StaffID; ?>">Edit</a>
                     </li>
                 <?php endforeach; }
             else{
@@ -17,15 +17,29 @@
             }
             ?>
         </ol>
+
+
+    <div id="delete">
+        <?php
+        if(isset($single_user)){
+            foreach ($single_user as $user): ?>
+                <h3>Sure you want to delete this user?</h3>
+                <?php echo $user->Email; ?>
+                <a href="<?php echo base_url() . 'index.php/Main/update/' . $user->StaffID; ?>">
+                    <button>Delete</button>
+                </a>
+            <?php endforeach; }
+        else{
+            echo 'No data found';
+        }
+        ?>
     </div>
-
-
 
     <div id="update">
         <?php
         if(isset($single_user)){
             foreach ($single_user as $item):
-                echo form_open('UManage_cntrl/update_user'); ?>
+                echo form_open('Main/update'); ?>
 
                 <?php echo form_label('Examiner ID:'); echo form_error('StaffID'); ?><br/>
                 <?php echo form_input(array('id' => 'StaffID', 'name' => 'StaffID', 'value' => $item->StaffID)); ?>
@@ -71,4 +85,53 @@
         ?>
     </div>
 
+    <div id="insert" class="col-sm-5">
+        <?php echo form_open('Main/update'); ?>
+        <h3>Create new user</h3><hr/>
+        <?php if (isset($message)) { ?>
+
+            <h3>Data inserted successfully</h3>
+            <br/>
+
+            <?php
+        }
+
+        echo form_label('Title:'); ?><?php echo form_error('Title'); ?><br/>
+        <?php echo form_input(array('id' => 'Title', 'name' => 'Title', 'placeholder' => 'Mr./Ms.')); ?>
+        <br/>
+
+        <?php echo form_label('First Name:'); ?><?php echo form_error('First_Name'); ?><br/>
+        <?php echo form_input(array('id' => 'First_Name', 'name' => 'First_Name', 'placeholder' => 'Name')); ?>
+        <br/>
+
+        <?php echo form_label('Last Name:'); ?><?php echo form_error('Last_Name'); ?><br/>
+        <?php echo form_input(array('id' => 'Last_Name', 'name' => 'Last_Name', 'placeholder' => 'Surname')); ?>
+        <br/>
+
+        <?php echo form_label('Email :'); ?><?php echo form_error('Email'); ?><br/>
+        <?php echo form_input(array('id' => 'Email', 'name' => 'Email', 'placeholder' => 'example@mail.com')); ?>
+        <br/>
+
+        <?php echo form_label('Password :'); ?><?php echo form_error('Password'); ?><br/>
+        <?php echo form_input(array('id' => 'Password', 'name' => 'Password', 'placeholder' => 'p@s$word')); ?>
+        <br/>
+
+        <?php echo form_label('Address :'); ?><?php echo form_error('Address'); ?><br/>
+        <?php echo form_input(array('id' => 'Address', 'name' => 'Address', 'placeholder' => 'Sesame Street')); ?>
+        <br/>
+
+        <?php echo form_label('Post code :'); ?><?php echo form_error('Postcode'); ?><br/>
+        <?php echo form_input(array('id' => 'Postcode', 'name' => 'Postcode', 'placeholder' => 'NW8 6IX')); ?>
+        <br/>
+
+        <?php echo form_label('Phone :'); ?><?php echo form_error('Telephone'); ?><br/>
+        <?php echo form_input(array('id' => 'Telephone', 'name' => 'Telephone', 'placeholder' => '070XXXXXXX')); ?>
+
+        <br />
+
+        <?php
+        echo form_submit(array('id' => 'submit', 'value' => 'Create'));
+        echo form_close();
+        ?>
+    </div>
 </div>
