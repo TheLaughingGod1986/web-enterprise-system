cea60b5a63b8fe1d4655a76b11ca9e1c8356105d
TheLaughingGod1986 <benoats@gmail.com>
2016-03-08 21:48:27 +0000

    test

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -2,7 +2,7 @@
 <ul class="nav navbar-inverse-light side-bar li-change">
     <li class="blue"> <img border="0" alt="boo" src="img/greenwich_logo.png" width="100" height="70"></li>
     <li class="grey"></li>
-    <li role="presentation" class="active"><a href="#">Home</a></li>
+    <li role="presentation"><?php echo anchor('main/index', 'Home'); ?></li>
     <li role="presentation"><a href="#">Reports</a></li>
     <li role="presentation"><a href="#">Write / View Response</a></li>
     <li class="grey"></li>
