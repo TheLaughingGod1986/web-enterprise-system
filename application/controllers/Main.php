9df5de0cb944dffcec1357db5f3720e9978dbfe6
TheLaughingGod1986 <benoats@gmail.com>
2016-03-16 14:41:06 +0000

    ben's naughty hack/elegent soloution

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -9,10 +9,10 @@ class Main extends CI_Controller
         $this->load->model('Update_model');
         $this->load->helper('array');
 
-//      $this->load->model('Update_model');
-//        $id = $this->uri->segment(3);
-//        $data['all_users'] = $this->Udpate_model->get_users();
-//        $data['single_user'] = $this->UManage_model->get_user_id($id);
+      $this->load->model('Update_model');
+        $id = $this->uri->segment(3);
+        $data['all_users'] = $this->Udpate_model->get_users();
+        $data['single_user'] = $this->UManage_model->get_user_id($id);
     }
 
     var $template = array();
@@ -41,15 +41,16 @@ class Main extends CI_Controller
 
     function update()
     {
-        $this->middle = 'pages/update_view'; // passing middle to function. change this for different views.
-        $this->layout();
+//        $this->middle = 'pages/update_view'; // passing middle to function. change this for different views.
+//        $this->layout();
 
         $id = 2;
         $data['all_users'] = $this->Update_model->get_users();
         $data['single_user'] = $this->Update_model->get_user_id($id);
-//        echo 'data variable: '.random_element($data22);
-//        $this->middle = 'pages/update_view.php'; // passing middle to function. change this for different views.
-//        $this->layout();
+// This is a hack, naughty Ben ....but it may work ... hehehe
+        $this->template['middle'] = $this->load->view ($this->middle = 'UManage_view',$data, $id, true);
+        $this->layout();
+    }
 
     }
 }
