fd27c469ee7934d3d5f193bf0b5a43478cec5aed
TheLaughingGod1986 <benoats@gmail.com>
2016-03-16 19:51:13 +0000

    new test 123

--- application/controllers/Update_cntrl.php
+++ application/controllers/Update_cntrl.php
@@ -6,6 +6,11 @@
  * Time: 11:24
  */
 class Update_cntrl extends CI_Controller{
+
+    var $data = array();
+    var $Front_End_data = array();
+    var $template = array();
+    
     function __construct()
     {
         parent::__construct();
@@ -25,5 +30,24 @@ class Update_cntrl extends CI_Controller{
         $this->db->where('StaffID', $data);
         $query = $this->db->get('staff');
         return $query->result();
+    }
+
+    public function layout()
+    {
+        $this->template['header'] = $this->load->view('layout/header', $this->Front_End_data, true);
+        $this->template['left'] = $this->load->view('layout/left', $this->Front_End_data, true);
+        $this->template['middle'] = $this->load->view($this->middle, $this->Front_End_data, true);
+        $this->load->view('layout/index', $this->template);
+    }
+
+    function index()
+    {
+        $this->load->model('Update_model');
+        $id = 2;
+        $data['all_users'] = $this->Update_model->get_users();
+        $data['single_user'] = $this->Update_model->get_user_id($id);
+
+        $this->template['middle'] = $this->load->view($this->middle = 'pages/update_view', $data, $id, true);
+        $this->layout();
     }
 }
