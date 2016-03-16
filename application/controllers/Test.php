e159ba0ac301b14acd6ff6ad708f1a159db577eb
TheLaughingGod1986 <benoats@gmail.com>
2016-03-16 22:32:26 +0000

    test layout logic

--- application/controllers/Test.php
+++
@@ -1,7 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Test extends Template_Controller {
-    public function index() {
-        $this->middle = 'pages/home_view'; // passing middle to function. change this for different views.
-        $this->layout();
-    }
-}
