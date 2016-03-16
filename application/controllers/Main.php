217ce0c17ea9add18e6c296f3101eeec34c45b70
TheLaughingGod1986 <benoats@gmail.com>
2016-03-16 18:58:34 +0000

    new test 123

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -32,9 +32,8 @@ class Main extends CI_Controller
         $data['all_users'] = $this->Update_model->get_users();
         $data['single_user'] = $this->Update_model->get_user_id($id);
 
-        
-
         $this->template['middle'] = $this->load->view($this->middle = 'pages/update_view', $data, $id, true);
-        $this->layout();
+        $this->template['left'] = $this->load->view('layout/left', $this->Front_End_data, true);
+        $this->template['middle'] = $this->load->view($this->middle, $this->Front_End_data, true);
     }
 }
