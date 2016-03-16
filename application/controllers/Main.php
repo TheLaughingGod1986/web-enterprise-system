561556ce68d1360093340e008048ff6d84ad7663
TheLaughingGod1986 <benoats@gmail.com>
2016-03-16 22:22:58 +0000

    test layout logic

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -24,8 +24,7 @@ class Main extends MY_Controller {
         $data['all_users'] = $this->Update_model->get_users();
         $data['single_user'] = $this->Update_model->get_user_id($id);
 
-        $this->data['all_users ,single_user'] = 'pages/update_view';
-        $this->middle = 'pages/home_view';
+        $this->middle = 'pages/update_view';
 //        $this->load->view($this->middle = 'pages/update_view', $data, $id, true);
         $this->layout();
     }
