3e63262974ce3a77b3cf3a6566c9989b73df06b2
TheLaughingGod1986 <benoats@gmail.com>
2016-03-16 15:04:50 +0000

    ben's naughty hack/elegent soloution

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,22 +3,19 @@
 class Main extends CI_Controller
 {
     var $data = array();
+
     function __construct()
     {
         parent::__construct();
         $this->load->model('Update_model');
         $this->load->helper('array');
-
-      $this->load->model('Update_model');
-        $id = $this->uri->segment(3);
-//        $data['all_users'] = $this->Udpate_model->get_users();
-//        $data['single_user'] = $this->UManage_model->get_user_id($id);
+//        $id = $this->uri->segment(3);
     }
 
     var $Front_End_data = array();
     var $template = array();
 
-    public function layout ()
+    public function layout()
     {
         $this->template['header'] = $this->load->view('layout/header', $this->Front_End_data, true);
         $this->template['left'] = $this->load->view('layout/left', $this->Front_End_data, true);
@@ -29,15 +26,11 @@ class Main extends CI_Controller
     {
         $this->middle = 'pages/home_view'; // passing middle to function. change this for different views.
         $this->layout();
-
     }
 
     function about()
     {
-//        $this->middle = 'pages/about_view'; // passing middle to function. change this for different views.
-//        $this->layout();
-
-        $this->template['middle'] = $this->load->view ($this->middle = 'pages/about_view', true);
+        $this->template['middle'] = $this->load->view($this->middle = 'pages/about_view', true);
         $this->layout();
     }
 
@@ -48,9 +41,7 @@ class Main extends CI_Controller
         $data['single_user'] = $this->Update_model->get_user_id($id);
 
 // This is a hack, naughty Ben ....but it may work ... hehehe
-        $this->template['middle'] = $this->load->view ($this->middle = 'UManage_view',$data, $id, true);
+        $this->template['middle'] = $this->load->view($this->middle = 'UManage_view', $data, $id, true);
         $this->layout();
     }
-
-
 }
