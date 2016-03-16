ffd6491e6cc78c812d3c650700ab122de7533f8b
TheLaughingGod1986 <benoats@gmail.com>
2016-03-16 18:50:30 +0000

    new test 123

--- application/views/layout/index.php
+++ application/views/layout/index.php
@@ -12,15 +12,18 @@
 
 <body>
 
-<div class="col-sm-2 no-padding-right">
-    <?php if ($left) echo $left; ?>
-</div>
 
-<div class="col-sm-10 no-padding-left">
-    <?php if ($header) echo $header; ?>
-</div>
+<?php if ($header) echo $header; ?>
 
-<div class="col-sm-10 ">
+<?php
+if ($this->session->userdata('id')) {
+    echo '<div class="col-sm-3 col-md-2 sidebar">';
+    if ($left) echo $left;
+    echo '</div>';
+}
+?>
+
+<div class="col-sm-9 push-top push-left">
     <?php if ($middle) echo $middle; ?>
 </div>
 
