6cd96f0d6cdc9bb49fad0fdb5c56cdf72de9aff5
TheLaughingGod1986 <benoats@gmail.com>
2016-03-16 22:01:52 +0000

    test layout logic

--- application/config/config.php
+++ application/config/config.php
@@ -513,3 +513,14 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+function __autoload($class)
+{
+    if (strpos($class, 'CI_') !== 0)
+    {
+        if (file_exists($file = APPPATH . 'core/' . $class . EXT))
+        {
+            include $file;
+        }
+    }
+}
