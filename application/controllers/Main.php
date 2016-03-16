048ac575d41c34fb3d94859a035b39759ec7a59b
TheLaughingGod1986 <benoats@gmail.com>
2016-03-16 23:40:17 +0000

    check this fix

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -27,7 +27,7 @@ class Main extends MY_Controller {
 //        $this->middle = 'pages/update_view';
 
         $data_tek['middle'] = 'pages/update_view';
-        $this->load->view('layout', $data_tek, $id, $data);
+        $this->load->view('layout/index', $data_tek, $id, $data);
 //        $this->load->view($this->middle = 'pages/update_view', $data, $id, true);
         $this->layout();
     }
