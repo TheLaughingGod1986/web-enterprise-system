f5ddd6f258a1fabb08026a2dd1144260f726dc74
TheLaughingGod1986 <benoats@gmail.com>
2016-03-16 18:38:17 +0000

    new test 123

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -16,7 +16,7 @@ class Main extends CI_Controller
     {
         $this->template['header'] = $this->load->view('layout/header', $this->Front_End_data, true);
         $this->template['left'] = $this->load->view('layout/left', $this->Front_End_data, true);
-//        $this->template['middle'] = $this->load->view($this->middle, $this->Front_End_data, true);
+        $this->template['middle'] = $this->load->view($this->middle, $this->Front_End_data, true);
         $this->load->view('layout/index', $this->template);
     }
 
@@ -33,7 +33,7 @@ class Main extends CI_Controller
         $data['single_user'] = $this->Update_model->get_user_id($id);
 
 // This is a hack, naughty Ben ....but it may work ... hehehe
-//        $this->template['middle'] = $this->load->view($this->middle = 'pages/update_view', $data, $id, true);
+        $this->template['middle'] = $this->load->view($this->middle = 'pages/update_view', $data, $id, true);
         $this->layout();
     }
 }
