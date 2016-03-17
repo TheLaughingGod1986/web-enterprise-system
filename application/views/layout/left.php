d5998ebb3a3b218280cce38524debe7c88c56332
TheLaughingGod1986 <benoats@gmail.com>
2016-03-17 03:27:35 +0000

    new link

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,11 +1,13 @@
-
 <ul class="nav navbar-inverse-light side-bar li-change">
-    <li class="blue"> <img style="height: 290%; width: 100%;" border="0" alt="Hi Ray" src="<?php echo base_url(); ?>img/Black&WhiteGreenwich.svg"></li>
+    <li class="blue"><img style="height: 290%; width: 100%;" border="0" alt="Hi Ray"
+                          src="<?php echo base_url(); ?>img/Black&WhiteGreenwich.svg"></li>
     <li class="grey"></li>
+
     <li role="presentation" class="active"><?php echo anchor('main/index', 'Home'); ?></li>
     <li role="presentation"><a href="#">Reports</a></li>
     <li role="presentation"><a href="#">Write / View Response</a></li>
     <li class="grey"></li>
+
     <li role="presentation"><a href="#">Your Response</a></li>
     <li role="presentation"><?php echo anchor('main/update', 'Update Personal Details'); ?></li>
     <li role="presentation"><a href="#">Write / View Response</a></li>
