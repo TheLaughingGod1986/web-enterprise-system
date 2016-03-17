d3a26c3a1260d1674e85a7be941e5ea511827d4f
TheLaughingGod1986 <benoats@gmail.com>
2016-03-17 00:33:35 +0000

    check this fix

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -3,13 +3,13 @@ class MY_Controller extends CI_Controller
 {
     //set the class variable.
     var $template  = array();
-    var $data      = array();
+    var $front_data      = array();
     //Load layout
     public function layout() {
         // making temlate and send data to view.
-        $this->template['header']   = $this->load->view('layout/header', $this->data, true);
-        $this->template['left']   = $this->load->view('layout/left', $this->data, true);
-        $this->template['middle'] = $this->load->view($this->middle, $this->data, true);
+        $this->template['header']   = $this->load->view('layout/header', $this->front_data, true);
+        $this->template['left']   = $this->load->view('layout/left', $this->front_data, true);
+//        $this->template['middle'] = $this->load->view($this->middle, $this->front_data, true);
         $this->load->view('layout/index', $this->template);
     }
 }
