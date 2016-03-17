2e59a0581ad9cb910c11f14fc6c93fbaa84abf86
TheLaughingGod1986 <benoats@gmail.com>
2016-03-17 14:56:20 +0000

    its getting closer

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,15 +1,31 @@
 <ul class="nav navbar-inverse-light side-bar li-change">
-    <li class="blue"><img style="height: 290%; width: 100%;" border="0" alt="Hi Ray"
-                          src="<?php echo base_url(); ?>img/Black&WhiteGreenwich.svg"></li>
+    <li class="blue">
+        <?php
+        $this->load->helper('html'); // Load HTML Helper for img()
+        $img = base_url('img/Black&WhiteGreenwich.svg'); // generates text: siteroot/img/servare.png
+        $path = 'main'; // link to home controller index
+        ?>
+        <div class="logo-img">
+            <?php
+            echo anchor($path, img($img));
+            ?>
+        </div>
+    </li>
+
     <li class="grey"></li>
 
     <li role="presentation" class="active"><?php echo anchor('main/index', 'Home'); ?></li>
+    <li role="presentation"><a href="#">Externals</a></li>
     <li role="presentation"><a href="#">Reports</a></li>
-    <li role="presentation"><a href="#">Write / View Response</a></li>
+    <li role="presentation"><a href="#">Responses</a></li>
+    <li role="presentation"><a href="#">Recommendations</a></li>
+    <li role="presentation"><a href="#">PSRB</a></li>
+    <li role="presentation"><a href="#">Analise Of Data</a></li>
+    <li role="presentation"><a href="#">Missing Reports</a></li>
+
     <li class="grey"></li>
 
-    <li role="presentation"><a href="#">Your Response</a></li>
     <li role="presentation"><?php echo anchor('main/update', 'Update Personal Details'); ?></li>
-    <li role="presentation"><a href="#">Write / View Response</a></li>
+    <li role="presentation"><a href="#">Change Login Details</a></li>
     <li role="presentation"><a href="#">Logout</a></li>
 </ul>
