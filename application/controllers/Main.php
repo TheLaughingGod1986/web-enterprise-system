b85cef14c5743e9480aed7bdcc13571ebb07f926
iniestar <ruifurtado24@gmail.com>
2016-03-17 00:57:39 +0000

    checking load view fun2

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -23,7 +23,7 @@ class Main extends MY_Controller {
         $data['all_users'] = $this->Update_model->get_users();
         $data['single_user'] = $this->Update_model->get_user_id($id);
 //
-//$this->middle = 'pages/update_view';
+        $this->middle = 'pages/update_view';
 
         //$this->load->view($this->middle = 'pages/update_view', $data);
         $this->layout();
