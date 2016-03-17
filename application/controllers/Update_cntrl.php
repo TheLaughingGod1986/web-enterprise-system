9d108ed53c58daba09f99dcd08720622bb186046
iniestar <ruifurtado24@gmail.com>
2016-03-17 14:43:17 +0000

    trying to extended the contoller so layout() can work

--- application/controllers/Update_cntrl.php
+++ application/controllers/Update_cntrl.php
@@ -1,10 +1,6 @@
 <?php
 
-class Update_cntrl extends CI_Controller{
-
-//    var $data = array();
-//    var $Front_End_data = array();
-//    var $template = array();
+class Update_cntrl extends MY_Controller{
 
     function __construct()
     {
@@ -47,23 +43,4 @@ class Update_cntrl extends CI_Controller{
         $this->layout();
     }
 
-//    public function layout()
-//    {
-//        $this->template['header'] = $this->load->view('layout/header', $this->Front_End_data, true);
-//        $this->template['left'] = $this->load->view('layout/left', $this->Front_End_data, true);
-//        $this->template['middle'] = $this->load->view($this->middle, $this->Front_End_data, true);
-//        $this->load->view('layout/index', $this->template);
-//    }
-
-//    function index()
-//    {
-//        $this->load->model('Update_model');
-//        $id = 2;
-//        $data['all_users'] = $this->Update_model->get_users();
-//        $data['single_user'] = $this->Update_model->get_user_id($id);
-//
-//        $this->template['middle'] = $this->load->view($this->middle = 'pages/update_view', $data, $id, true);
-////        $this->load->view($this->middle = 'pages/update_view', $data, $id, true);
-//        $this->layout();
-//    }
 }
