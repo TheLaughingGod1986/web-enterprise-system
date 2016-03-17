eca2643f30656dbf70131d041defe8d8fe0cb2e6
TheLaughingGod1986 <benoats@gmail.com>
2016-03-17 18:36:36 +0000

    sass time woop part 7

--- application/views/layout/top_bar.php
+++ application/views/layout/top_bar.php
@@ -1,6 +1,6 @@
 <nav class="navbar nav-second   no-margin-bottom">
-            <h4 class="float-left">Hi, User</h4>
-            <h4 class="float-right">Select date drop down</h4>
+            <h4 class="float-left push-left-small">Hi, User</h4>
+            <h4 class="float-right push-right-small">Select date drop down</h4>
     </div>
 </nav>
 
