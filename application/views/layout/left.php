53ff07f45c37c75a4b0b2eb93ff9797f2eaeac1d
TheLaughingGod1986 <benoats@gmail.com>
2016-03-17 03:11:09 +0000

    one more time

--- application/views/layout/left.php
+++ application/views/layout/left.php
@@ -1,6 +1,6 @@
 
 <ul class="nav navbar-inverse-light side-bar li-change">
-    <li class="blue"> <img style="height: 290%; width: 47%;" border="0" alt="Hi Ray" src="<?php echo base_url(); ?>img/Black&WhiteGreenwich.svg"></li>
+    <li class="blue"> <img style="height: 290%; width: 100%;" border="0" alt="Hi Ray" src="<?php echo base_url(); ?>img/Black&WhiteGreenwich.svg"></li>
     <li class="grey"></li>
     <li role="presentation" class="active"><a href="#">Home</a></li>
     <li role="presentation"><a href="#">Reports</a></li>
