4d9c1df592c5a2150c8ce7351bb2414423b323e5
TheLaughingGod1986 <benoats@gmail.com>
2016-03-18 17:59:22 +0000

    add login model

--- application/controllers/Login_cntrl.php
+++ application/controllers/Login_cntrl.php
@@ -22,7 +22,7 @@ class Login_cntrl extends CI_Controller
         // read user's credentials from db, through Login Model
         if (isset($userlogged)) {
             $this->session->set_userdata('login_state', TRUE);
-            redirect('/UManage_view');
+            redirect('pages/home_view');
         } else {
             $this->load->view('A_login_view');    // redirect back to login page
         }
