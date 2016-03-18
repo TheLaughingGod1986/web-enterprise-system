d6bd2ef3ca95b02b27e22860b1bfc63149e39d2b
iniestar <ruifurtado24@gmail.com>
2016-03-18 17:57:20 +0000

    some validations for update users

--- application/controllers/Update_cntrl.php
+++ application/controllers/Update_cntrl.php
@@ -28,24 +28,77 @@ class Update_cntrl extends MY_Controller{
     //Update users... fetching user from database by id
     function update_user()
     {
-        $id = $this->input->post('StaffID');
-        $data = array(
-            'Email' => $this->input->post('Email'),
-            'Password' => $this->input->post('Password'),
-            'First_Name' => $this->input->post('First_Name'),
-            'Last_Name' => $this->input->post('Last_Name'),
-            'Postcode' => $this->input->post('Postcode'),
-            'Telephone' => $this->input->post('Telephone'),
-            'Address' => $this->input->post('Address'),
-            'Title' => $this->input->post('Title')
+        $this->load->library('form_validation');
+
+
+        $config = array(
+          array(
+              'field' => 'firstname',
+              'label' => 'Firstname',
+              'rules' => 'required'
+          ),
+            array(
+                'field' => 'lastname',
+                'label' => 'lastname',
+                'rules' => 'required'
+            ),
+            array(
+                'field' => 'Title',
+                'label' => 'Title',
+                'rules' => 'required'
+            ),
+            array(
+              'field' => 'Email',
+                'lable' => 'Email',
+                'rules' => 'required|valid_email|is_unique[all_users.email]'
+            ),
+            array(
+                'field' => 'password',
+                'label' => 'Password',
+                'rules' => 'required',
+                array(
+                    'required' => 'You must provide a %s.',
+                ),
+            ),
+            array(
+                'field' => 'passConfirm',
+                'label' => 'Password Confirmation',
+                'rules' => 'required|matches[Password]'
+            )
         );
 
-        $this->Update_model->update_user($id, $data);
+        $this->form_validation->set_rules($config);
+
+        if($this->form_validation->run() == false){
+
+            $id = $this->uri->segment(3);
+            $data['all_users'] = $this->Update_model->get_users();
+            $data['single_user'] = $this->Update_model->get_user_id($id);
+
+            $this->template['middle'] = $this->load->view ($this->middle = 'pages/update_view',$data, true);
+            $this->layout();
+
+        } else {
+            $id = $this->input->post('StaffID');
+            $data = array(
+                'Email' => $this->input->post('Email'),
+                'Password' => $this->input->post('Password'),
+                'First_Name' => $this->input->post('First_Name'),
+                'Last_Name' => $this->input->post('Last_Name'),
+                'Postcode' => $this->input->post('Postcode'),
+                'Telephone' => $this->input->post('Telephone'),
+                'Address' => $this->input->post('Address'),
+                'Title' => $this->input->post('Title')
+            );
+
+            $this->Update_model->update_user($id, $data);
             $id = $this->uri->segment(3);
             $data['all_users'] = $this->Update_model->get_users();
             $data['single_user'] = $this->Update_model->get_user_id($id);
 //        // This is a hack, naughty Ben ....but it may work ... hehehe
-        $this->template['middle'] = $this->load->view ($this->middle = 'pages/update_view',$data, true);
-        $this->layout();
+            $this->template['middle'] = $this->load->view ($this->middle = 'pages/update_view',$data, true);
+            $this->layout();
+        }
+
     }
 }
