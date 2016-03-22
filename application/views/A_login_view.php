6b88d0fd4a82788fbb7f3576efa7c99144b5ff02
iniestar <ruifurtado24@gmail.com>
2016-03-22 12:45:37 +0000

    come on...

--- application/views/A_login_view.php
+++ application/views/A_login_view.php
@@ -4,11 +4,11 @@ echo form_open('Login_cntrl/login');
 
 echo form_label('E-mail:', 'email');
 echo form_error('email');
-echo form_input('email', 'id = "email"', 'placeholder = "example@gre.ac.uk"');
+echo form_input('email', '', 'id = "email"', 'placeholder = "example@gre.ac.uk"');
 
 echo form_label('Password:', 'password');
 echo form_error('password');
-echo form_password('password','id = "password"');
+echo form_password('password', '', 'id = "password"');
 
 echo form_submit('submit', 'Login');
 echo form_close();
