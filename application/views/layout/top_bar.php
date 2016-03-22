e5fc4c670c685490d9f9efce3ce1ca22149e0c4f
iniestar <ruifurtado24@gmail.com>
2016-03-22 15:52:58 +0000

    on my whay to even

--- application/views/layout/top_bar.php
+++ application/views/layout/top_bar.php
@@ -1,5 +1,5 @@
 <nav class="navbar nav-second   no-margin-bottom">
-            <h4 class="float-left push-left-small">Hi, User</h4>
+            <h4 class="float-left push-left-small">Hi, <?php $this->session->name; ?></h4>
             <h4 class="float-right push-right-small">Select date drop down</h4>
     </div>
 </nav>
