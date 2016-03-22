aa54068a830a9c18a86cfcfef121e2755d32f211
TheLaughingGod1986 <benoats@gmail.com>
2016-03-22 16:23:58 +0000

    check, update

--- application/views/layout/top_bar.php
+++ application/views/layout/top_bar.php
@@ -1,6 +1,15 @@
 <nav class="navbar nav-second   no-margin-bottom">
-            <h4 class="float-left push-left-small">Hi, <?php echo $this->session->name; ?></h4>
-            <h4 class="float-right push-right-small">Select date drop down</h4>
+    <?php
+    if ($this->session->logged_in) {
+        echo "<h4 class='float-left push-left-small'>Welcome back,";
+        echo $this->session->name;
+        echo "!</h4>";
+    }
+    {
+        echo "Welcome, Please Login.";
+    }
+    ?>
+    <h4 class="float-right push-right-small">Select date drop down</h4>
     </div>
 </nav>
 
--- application/views/pages/home_view.php
+++ application/views/pages/home_view.php
@@ -1,14 +1,23 @@
 <?php
+if ($this->session->logged_in) {
 
-echo form_open('Login_cntrl/login');
+ echo "Welcome back", $this->session->name;
+}
 
-echo form_label('E-mail:');
-echo form_error('email');
-echo form_input(array('id' => 'email', 'name' => 'email', 'placeholder' => 'example@gre.ac.uk'));
+else {
+    echo form_open('Login_cntrl/login');
 
-echo form_label('Password:');
-echo form_error('password');
-echo form_input(array('id' => 'password', 'name' => 'password', 'placeholder' => 'password'));
+    echo form_label('E-mail:');
+    echo form_error('email');
+    echo form_input(array('id' => 'email', 'name' => 'email', 'placeholder' => 'example@gre.ac.uk'));
 
-echo form_submit(array('id' => 'submit', 'value' => 'Login'));
-echo form_close();
+    echo form_label('Password:');
+    echo form_error('password');
+    echo form_input(array('id' => 'password', 'name' => 'password', 'placeholder' => 'password'));
+
+    echo form_submit(array('id' => 'submit', 'value' => 'Login'));
+    echo form_close();
+}
+
+
+
