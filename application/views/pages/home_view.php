eb1c55c0cf702ffa7da06e5dd052976e178fed4f
TheLaughingGod1986 <benoats@gmail.com>
2016-03-23 03:11:04 +0000

    sepreat logins

--- application/views/pages/home_view.php
+++ application/views/pages/home_view.php
@@ -1,12 +1,41 @@
 <?php
 if ($this->session->logged_in) {
 
- echo "Hello";
+ echo "Hello" ;
     echo $this->session->name;
     echo "chose a option from the menu";
 }
 
 else {
+    echo "Admin Login";
+    echo form_open('Login_cntrl/login');
+
+    echo form_label('E-mail:');
+    echo form_error('email');
+    echo form_input(array('id' => 'email', 'name' => 'email', 'placeholder' => 'example@gre.ac.uk'));
+
+    echo form_label('Password:');
+    echo form_error('password');
+    echo form_input(array('id' => 'password', 'name' => 'password', 'placeholder' => 'password'));
+
+    echo form_submit(array('id' => 'submit', 'value' => 'Login'));
+    echo form_close();
+
+    echo "Staff Login";
+    echo form_open('Login_cntrl/login');
+
+    echo form_label('E-mail:');
+    echo form_error('email');
+    echo form_input(array('id' => 'email', 'name' => 'email', 'placeholder' => 'example@gre.ac.uk'));
+
+    echo form_label('Password:');
+    echo form_error('password');
+    echo form_input(array('id' => 'password', 'name' => 'password', 'placeholder' => 'password'));
+
+    echo form_submit(array('id' => 'submit', 'value' => 'Login'));
+    echo form_close();
+
+    echo "External Examiner Login";
     echo form_open('Login_cntrl/login');
 
     echo form_label('E-mail:');
