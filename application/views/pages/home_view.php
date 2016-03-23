6b316a6e4f59b4cd8c68202dad72ae527d9ce34f
TheLaughingGod1986 <benoats@gmail.com>
2016-03-23 20:41:10 +0000

    check 123

--- application/views/pages/home_view.php
+++ application/views/pages/home_view.php
@@ -14,7 +14,7 @@ else {
 ///bens start
     echo form_open('Login_cntrl/admin_login');
     echo form_input('Username', 'Username');
-    echo form_password('password', 'Password');
+    echo form_password('Password', 'Password');
     echo form_submit('submit', 'Login');
     //bens end
 
