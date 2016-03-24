e1907366a72837ce32873db8e6352eca4ce8c9c5
TheLaughingGod1986 <benoats@gmail.com>
2016-03-24 09:49:35 +0000

    add diffrent login banner to grey bar

--- application/views/layout/top_bar.php
+++ application/views/layout/top_bar.php
@@ -1,10 +1,23 @@
 <nav class="navbar nav-second   no-margin-bottom">
     <?php
     if ($this->session->is_logged_admin) {
-        echo "<h4 class='float-left push-left-small'>Welcome back,";
+        echo "<h4 class='float-left push-left-small'>Welcome back, ";
         echo $this->session->Username;
         echo "!</h4>";
     }
+
+   else if ($this->session->is_logged_external) {
+        echo "<h4 class='float-left push-left-small'>Welcome back, ";
+        echo $this->session->Email;
+        echo "!</h4>";
+    }
+
+   else if ($this->session->is_logged_staffl) {
+       echo "<h4 class='float-left push-left-small'>Welcome back, ";
+       echo $this->session->Email;
+       echo "!</h4>";
+   }
+   
    else {
        echo "<h4 class='float-left push-left-small'>Welcome, Please Login.";
     }
