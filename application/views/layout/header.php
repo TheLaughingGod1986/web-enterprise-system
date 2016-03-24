3c1d148778140dc9c691144bcd63b115e5b2da64
TheLaughingGod1986 <benoats@gmail.com>
2016-03-24 01:52:09 +0000

    test that it works

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,8 +1,11 @@
 <nav class="navbar navbar-inverse top-menu no-margin-bottom">
         <span class="top-center-text">
            <?php if ($this->session->is_logged_admin) {
-
                echo "<h1> Admin Portal - Faculty </h1>";
+           } else if ($this->session->is_logged_external) {
+               echo "<h1> External Portal - Faculty </h1>";
+           } else if ($this->session->is_logged_staff) {
+               echo "<h1> Staff Portal - Faculty </h1>";
            } else {
                echo "<h1> Please Login </h1>";
            } ?>
