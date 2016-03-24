62be441c60f5a402bc5481361306dca33191bd69
TheLaughingGod1986 <benoats@gmail.com>
2016-03-24 03:51:37 +0000

    that did not work

--- application/models/Authenticator.php
+++ application/models/Authenticator.php
@@ -11,20 +11,21 @@ class Authenticator extends CI_Model
     public function checkLogin()
     {
         $logged_admin = $this->session->is_logged_admin;
-        $logged_external = $this->session->is_logged_external;
-        $logged_staff = $this->session->is_logged_staff;
+//        $logged_external = $this->session->is_logged_external;
+//        $logged_staff = $this->session->is_logged_staff;
 
             if(!isset($logged_admin) || $logged_admin != TRUE) {
                 echo 'Im sorry, you do not have administrator pillages';
                 echo anchor('main/index', 'Return home');
+                die();
            }
-            else if(!isset($logged_external) || $logged_external != TRUE) {
-                echo 'Im sorry, you do not have external pillages';
-                echo anchor('main/index', 'Return home');
-            }
-            else if(!isset($logged_staff) || $logged_staff != TRUE) {
-                echo 'Im sorry, you do not have staff pillages';
-                echo anchor('main/index', 'Return home');
-            }
+//            else if(!isset($logged_external) || $logged_external != TRUE) {
+//                echo 'Im sorry, you do not have external pillages';
+//                echo anchor('main/index', 'Return home');
+//            }
+//            else if(!isset($logged_staff) || $logged_staff != TRUE) {
+//                echo 'Im sorry, you do not have staff pillages';
+//                echo anchor('main/index', 'Return home');
+//            }
     }
 }
