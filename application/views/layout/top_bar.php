3041c9baea78d272fedee576e9c88d20a0267ff0
TheLaughingGod1986 <benoats@gmail.com>
2016-03-24 09:55:06 +0000

    add diffrent login banner to grey bar

--- application/views/layout/top_bar.php
+++ application/views/layout/top_bar.php
@@ -4,24 +4,28 @@
         echo "<h4 class='float-left push-left-small'>Welcome back, ";
         echo $this->session->Username;
         echo "!</h4>";
+        echo "<h4 class='float-right push-right-small'>Select date drop down</h4>";
     }
 
    else if ($this->session->is_logged_external) {
         echo "<h4 class='float-left push-left-small'>Welcome back, ";
         echo $this->session->Email;
         echo "!</h4>";
+       echo "<h4 class='float-right push-right-small'>Select date drop down</h4>";
     }
 
    else if ($this->session->is_logged_staff) {
        echo "<h4 class='float-left push-left-small'>Welcome back, ";
        echo $this->session->Email;
        echo "!</h4>";
+       echo "<h4 class='float-right push-right-small'>Select date drop down</h4>";
    }
 
    else {
        echo "<h4 class='float-left push-left-small'>Welcome, Please Login.";
     }
     ?>
+
     </div>
 </nav>
 
