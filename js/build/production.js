6a22f08c472d58571dfc86527d5498eec10727f3
iniestar <ruifurtado24@gmail.com>
2016-03-24 18:15:53 +0000

    testestestestestestestestestes

--- js/build/production.js
+++ js/build/production.js
@@ -2433,6 +2433,6 @@ function updateList(){
             x.options.add(new Option(xhttp.DepartmentID, xhttp.Department_Name));
         }
     }
-    xhttp.open('POST','index.php/UManage_cntrl/ajaxTry',true);
+    xhttp.open('POST','/index.php/UManage_cntrl/ajaxTry',true);
     xhttp.send();
 }
