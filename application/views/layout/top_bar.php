37515c4aab89e67be70c55d79e52526baa4caab0
TheLaughingGod1986 <benoats@gmail.com>
2016-03-24 09:53:04 +0000

    add diffrent login banner to grey bar

--- application/views/layout/top_bar.php
+++ application/views/layout/top_bar.php
@@ -22,7 +22,6 @@
        echo "<h4 class='float-left push-left-small'>Welcome, Please Login.";
     }
     ?>
-    <h4 class="float-right push-right-small">Select date drop down</h4>
     </div>
 </nav>
 
