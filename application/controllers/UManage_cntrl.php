001715f8e7cfec996388d43bb97ed36d2012ffc7
Yussuf Elarif <Yussuf Elarif>
2016-03-31 17:25:28 +0100

    sAFasdfaiosjfoaijfosidjfsoidfjsdf

--- application/controllers/UManage_cntrl.php
+++ application/controllers/UManage_cntrl.php
@@ -157,11 +157,14 @@ class UManage_cntrl extends MY_Controller
 
             //Insert into token table
             //$this->UManage_model->insert_token($data->Email, 'token');
+            $email = $this->input->post('Email');
+            $pass = $this->input->post('Password');
+
             $this->email->from('noreply@benoats.co', 'University of Greenwich');
-			$this->email->to($data['Email']);
+			$this->email->to($email);
 
 			$this->email->subject('Email Verification & Password Update');
-			$this->email->message('Email:' . $data['Email'] . '\nPassword:' . $data['Password'] . '\n\nThe link below will direct you to where you will be able to update your password to ensure you do not forget it\n A LINK');
+			$this->email->message('Email:' . $email . '\nPassword:' . $pass . '\n\nThe link below will direct you to where you will be able to update your password to ensure you do not forget it\n A LINK');
 
 			$this->email->send();
 
@@ -186,13 +189,16 @@ class UManage_cntrl extends MY_Controller
             $this->UManage_model->insert_user($data, 'external');
             $data['message'] = 'External Examiner Created Successfully';
 
+            $email = $this->input->post('Email');
+            $pass = $this->input->post('Password');
+
             $this->email->from('noreply@benoats.co', 'University of Greenwich');
-			$this->email->to($data->Email); 
+            $this->email->to($email);
 
-			$this->email->subject('Email Verification & Password Update');
-			$this->email->message('Email:' . $data->Email . '\nPassword:' . $data->Password . '\n\nThe link below will direct you to where you will be able to update your password to ensure you do not forget it\n A LINK');	
+            $this->email->subject('Email Verification & Password Update');
+            $this->email->message('Email:' . $email . '\nPassword:' . $pass . '\n\nThe link below will direct you to where you will be able to update your password to ensure you do not forget it\n A LINK');
 
-			$this->email->send();
+            $this->email->send();
 
             //Loading View
             $this->template['middle'] = $this->load->view($this->middle = 'pages/createUser_view',$data, true);
