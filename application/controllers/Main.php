e8e77aed006f61012c288edb184e95e25a3cbcf5
TheLaughingGod1986 <benoats@gmail.com>
2016-04-26 17:28:55 +0100

    test report

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -28,7 +28,7 @@ class Main extends MY_Controller
 
             if ($query = $this->report_model->create_report()) {
 
-                $this->middle = '<script> prompt ("Congratulations, You Completed Your Report");</script>';
+                $this->middle = '<script>alert("Successful Message"); location.reload();)</script>';
                 $this->layout();
 
             } else {
