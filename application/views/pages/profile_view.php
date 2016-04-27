9024e30216350ac7a8b5ae00e45f7961c7c194df
Yussuf Elarif <Yussuf Elarif>
2016-04-27 17:17:48 +0100

    Fixed some last minute things

--- application/views/pages/profile_view.php
+++ application/views/pages/profile_view.php
@@ -2,8 +2,7 @@
 	//if ($this->session->is_logged_external || $this->session->is_logged_staff) {}
     if(isset($single_user)){
             echo '<li>';
-            echo $users->Last_Name.', '.$users->First_Name;
-            echo '<a href="' . base_url() . 'index.php/UManage_cntrl/index/' . $users->StaffID . '">Edit</a>';
+            echo $single_user->Last_Name.', '.$single_user->First_Name;
             echo '</li>';
     else{
         echo 'No Records';
