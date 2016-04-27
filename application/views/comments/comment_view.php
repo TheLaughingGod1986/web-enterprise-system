f0febd9518ef1137a469de64824d5961a51d76db
TheLaughingGod1986 <benoats@gmail.com>
2016-04-27 01:23:49 +0100

    comments getting there

--- application/views/comments/comment_view.php
+++ application/views/comments/comment_view.php
@@ -1,3 +1,13 @@
 <div class="col-sm-4">
 <h1>comments</h1>
+    <?=form_open('main/comment_add');?>
+
+    <?=form_hidden('ReportID', $this->uri->segment(3));?>
+
+    <p><textarea name="body" rows="10"></textarea></p>
+    <p><input type="text" name="author" /> </p>
+    <p><input type="submit" value="add comment" /></p>
+    
+
+    </form>
 </div>
