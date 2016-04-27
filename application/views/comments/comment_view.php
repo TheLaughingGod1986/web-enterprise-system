acf081099f36d24fd21d5c4f9a8a497d630897e5
TheLaughingGod1986 <benoats@gmail.com>
2016-04-27 01:24:55 +0100

    comments getting there

--- application/views/comments/comment_view.php
+++ application/views/comments/comment_view.php
@@ -1,5 +1,5 @@
 <div class="col-sm-4">
-<h1>comments</h1>
+<h1>comments oooh big boy</h1>
     <?=form_open('main/comment_add');?>
 
     <?=form_hidden('ReportID', $this->uri->segment(3));?>
@@ -7,7 +7,7 @@
     <p><textarea name="body" rows="10"></textarea></p>
     <p><input type="text" name="author" /> </p>
     <p><input type="submit" value="add comment" /></p>
-    
+
 
     </form>
 </div>
