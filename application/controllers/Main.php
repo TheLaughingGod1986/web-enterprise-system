2f465a39f8a51bbf1a20f572f2b77c65e6f1ed1e
TheLaughingGod1986 <benoats@gmail.com>
2016-04-27 03:27:03 +0100

    next step with flash

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -70,7 +70,7 @@ $this->db->where('ReportID', $this->uri->segment(3));
     {
         $this->db->insert('Report_Comments', $_POST);
 
-        $this->session->set_flashdata('messagetw', 'You added a Report');
+        $this->session->set_flashdata('messagetwo', 'You added a Report');
         redirect('main/comments/' .$_POST['ReportID']);
     }
     function externals()
