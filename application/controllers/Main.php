487edf2ed079596c67d708676affbcf2654f7aef
TheLaughingGod1986 <benoats@gmail.com>
2016-04-28 18:04:22 +0100

    add comments to staff

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -68,9 +68,7 @@ class Main extends MY_Controller
 
     function comment_add()
     {
-        if ($this->session->is_logged_admin) {
-
-            if ($query = $this->report_model->create_comment_admin()) {
+            if ($query = $this->report_model->create_comment()) {
 
                 $this->session->set_flashdata('messagetwo', 'You added a Comment');
                 redirect('main/comments/' .$_POST['ReportID']);
@@ -79,20 +77,6 @@ class Main extends MY_Controller
                 $this->session->set_flashdata('messagetwo', 'Sorry not this time');
                 redirect('main/comments/' .$_POST['ReportID']);
             }
-        }
-
-        else if (!isset($this->session->is_logged_staff) && $this->session->RoleID =='1') {
-            if ($query = $this->report_model->create_comment_staff()) {
-
-                $this->session->set_flashdata('messagetwo', 'You added a Comment');
-                redirect('main/comments/' .$_POST['ReportID']);
-
-            } else {
-                $this->session->set_flashdata('messagetwo', 'Sorry not this time');
-                redirect('main/comments/' .$_POST['ReportID']);
-            }
-        }
-
     }
 
     function externals()
