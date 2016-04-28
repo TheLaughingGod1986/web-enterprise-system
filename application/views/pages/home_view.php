27e2105b16d4b5fc510a6d7b7867173859de00ba
TheLaughingGod1986 <benoats@gmail.com>
2016-04-28 15:25:57 +0100

    test

--- application/views/pages/home_view.php
+++ application/views/pages/home_view.php
@@ -126,25 +126,24 @@ if ($this->session->is_logged_admin) {
     </div>
     <div class="col-sm-6">
         <h2>Latest Reports</h2>
-        <table style="width:100%">
-            <tr>
-                <th>Report</th>
-                <th>Report Link</th>
-            </tr>
+
 
         <?php if (isset($reports)) : foreach ($reports as $row) : ?>
+            <table border="1" style="width:50%">
                 <tr>
+                    <th>Report Name</th>
+                    <th>Link</th>
+                </tr>
+                <tr>
+                <tr>
+                    <td><?php echo $row->Report_Name; ?></td>
+                    <td><?= anchor('main/comments/' . $row->ReportID, 'Comments'); ?></td>
+                </tr>
+            </table>
                     <td><?php echo $row->Report_Name; ?></td>
                     <td><?= anchor('main/comments/' . $row->ReportID, 'Comments'); ?></td>
 
 
-
-                        <tr>
-                        <tr>
-                            <td><?php echo $row->Report_Name; ?></td>
-                            <td><?= anchor('main/comments/' . $row->ReportID, 'Comments'); ?></td>
-                        </tr>
-                    </table>
             <hr>
         <?php endforeach; ?>
         <?php else : ?>
