fadd70ba22f8f3f98618e0a17c8f36a41ae8cc4b
Yussuf Elarif <Yussuf Elarif>
2016-04-28 23:52:58 +0100

    changes

--- application/views/pages/profile_view.php
+++ application/views/pages/profile_view.php
@@ -1,9 +1,13 @@
 <?php
 	if ($profile != null && $others == true){
 		//Show profile based on segment 3
-        //echo $profile[0]->Email;
-        echo "other records";
-
+        echo '<div style="border-bottom:1px solid #BFBFBF;padding:15px;padding-bottom:20px;position:relative;">';
+		echo '<img src="http://cumbrianrun.co.uk/wp-content/uploads/2014/02/default-placeholder.png" width="100"/>';
+		echo '<div style="display:inline-block;padding-left:10px;position:absolute;top:15px;">';
+		echo '<div>' . $profile[0]->First_Name . ', ' . $profile[0]->Last_Name . '</div>';
+		echo '<div>' . $profile[0]->Email . '</div>';
+		echo '</div>';	
+		echo '</div>';
 	}
 	else if ($profile == null && $others == false){
 		//Show personal profile
