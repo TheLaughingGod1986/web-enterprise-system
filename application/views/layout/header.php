9c0167896c4be500e67d35484f903e1f4aa9b6d5
gfarrar <fg201@greenwich.ac.uk>
2016-04-29 19:53:05 +0100

    correcting spelling

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,4 +1,5 @@
 <nav class="navbar navbar-inverse top-menu no-margin-bottom">
+    <div>
         <span class="top-center-text">
            <?php
            if ($this->session->is_logged_admin) {
@@ -6,19 +7,19 @@
            }
 
            else if ($this->session->is_logged_external) {
-               echo "<h1>External Portal - Faculty </h1>";
+               echo "<h1>External Examiner Portal - Faculty </h1>";
            }
 
            else if (!isset($this->session->is_logged_staff) && $this->session->RoleID =='1') {
-               echo "<h1>Program leader Portal - Faculty</h1>";
+               echo "<h1>Programme Leader Portal - Faculty</h1>";
            }
 
            else if (!isset($this->session->is_logged_staff) && $this->session->RoleID =='2') {
-               echo "<h1>Pro vice chancellery Portal - Faculty</h1>";
+               echo "<h1>Pro-Vice Chancellor Portal - Faculty</h1>";
            }
 
            else if (!isset($this->session->is_logged_staff, $this->session->RoleID ) && $this->session->RoleID =='3') {
-               echo "<h1>Director of learning and qualit Portal - Faculty</h1>";
+               echo "<h1>Director of Learning and Teaching Portal - Faculty</h1>";
            }
 
            else {
