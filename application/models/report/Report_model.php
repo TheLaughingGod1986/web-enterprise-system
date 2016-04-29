1aad7fa9629b06e3a26d7e40ee09d6ce2bf76d5a
TheLaughingGod1986 <benoats@gmail.com>
2016-04-29 08:44:53 +0100

    add comments to staff

--- application/models/report/Report_model.php
+++ application/models/report/Report_model.php
@@ -97,7 +97,7 @@ class Report_model extends CI_Model
         $this->db->select('SELECT report.Report_name, report.ReportDate, report.ReportId')
             ->from('report')
             ->join('report LEFT JOIN Read_Report ON report.ReportId=Read_Report.ReportId')
-            ->where('Read_Report.ReportId IS NULL', $this->session->userdata("StaffID"));
+            ->where('Read_Report.ReportId IS NULL');
 
         $result = $this->db->get();
         return $result->result();
