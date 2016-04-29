78fcb14e1658dd30d07efa6339d4c02b028c386f
TheLaughingGod1986 <benoats@gmail.com>
2016-04-29 01:50:27 +0100

    add comments to staff

--- application/views/pages/reports_view.php
+++ application/views/pages/reports_view.php
@@ -5,6 +5,7 @@
         <th><h3>Report Title</h3></th>
         <th><h3>Report Date</h3></th>
         <th><h3>See Full Report</h3></th>
+        <th><h3>Read Report</h3></th>
     </tr>
     </thead>
     <tbody>
@@ -17,6 +18,7 @@
         <td><b><?php echo $row->Report_Name; ?></b></td>
         <td><b><?php echo $row->ReportDate; ?></b></td>
         <td><b><?= anchor('main/comments/' . $row->ReportID, 'Full Report'); ?></b></td>
+        <td><input type="checkbox"name="businessType1" value="1"></td>
     </tr>
 
     <?php endforeach; ?>
