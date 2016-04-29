c9a2c9898ac0a08754a3353403c2c871f906025d
TheLaughingGod1986 <benoats@gmail.com>
2016-04-29 18:38:54 +0100

    put-old-repot-creat-back

--- application/models/report/Report_model.php
+++ application/models/report/Report_model.php
@@ -97,7 +97,7 @@ class Report_model extends CI_Model
         $this->db->select('report.Report_Name, report.ReportDate, report.ReportID')
             ->from('report')
             ->join('Read_Report', 'report.ReportID = Read_Report.ReportID', 'left')
-            ->where('Read_Report.ReportID IS NULL')
+            ->where('Read_Report.ReportID IS NULL');
 //            ->where('Read_Report.StaffID',$this->session->userdata("StaffID"));
         $result = $this->db->get();
         return $result->result();
