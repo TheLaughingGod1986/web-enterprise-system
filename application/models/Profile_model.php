0bee22c8dcd3a2a8b8e3a1e4026b11e037953652
Yussuf Elarif <Yussuf Elarif>
2016-04-30 02:31:17 +0100

    changes

--- application/models/Profile_model.php
+++ application/models/Profile_model.php
@@ -18,7 +18,7 @@ class Profile_model extends CI_Model{
     }
 
     function get_messages($type, $id){
-        $data = "To = '$id' AND To_Type = '$type'";
+        $data = "`To` = '$id' AND To_Type = '$type'";
         $this->db->where($data, NULL, FALSE);
         $query = $this->db->get('message');
         return $query->result();
