e23dd101c8a5b4b1c22e3731b7f91e12184e35f8
iniestar <ruifurtado24@gmail.com>
2016-05-01 17:52:58 +0100

    test

--- application/views/pages/home_view.php
+++ application/views/pages/home_view.php
@@ -42,7 +42,7 @@ if ($this->session->is_logged_admin) {
         <tr>
             <td><b><?php echo $row->Report_Name; ?></b></td>
             <td><b><?php echo $row->ReportDate ?></b></td>
-            <td><button type="button" class="btn btn-success">View<?= anchor('main/comments/' . $row->ReportID, 'Full Report'); ?></button></td>
+            <td><button type="button" class="btn btn-success"><?= anchor('main/comments/' . $row->ReportID, 'View Report'); ?></button></td>
         </tr>
 
         <?php endforeach; ?>
@@ -78,7 +78,7 @@ if ($this->session->is_logged_admin) {
         <tr>
             <td><b><?php echo $row->Report_Name; ?></b></td>
             <td><b><?php echo $row->ReportDate; ?></b></td>
-            <td><button type="button" class="btn btn-success">View<?= anchor('main/comments/' . $row->ReportID, 'Full Report'); ?></button></td>
+            <td><button type="button" class="btn btn-success"><?= anchor('main/comments/' . $row->ReportID, 'View Report'); ?></button></td>
         </tr>
 
         <?php endforeach; ?>
